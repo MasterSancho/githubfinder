06fc2a485c84cb72cb3a7d4cebb929136926ee04
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-04-30 19:09:22 +0300

    Passing Props Up & Search
    1-Connect Search to api
    2-Create searchUsers function and make it work

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Search extends Component {
   state = {
     text: "",
   };
 
-  onSubmit = (e) => {
-    e.preventDefault()
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  };
 
-    console.log(this.state.text);
-  }
-  
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
+  };
+
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
